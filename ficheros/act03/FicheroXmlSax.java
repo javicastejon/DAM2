6a72f87b8b7f34d45568812b6d00c70f4ceae3b8
José Javier Castejón Sánchez <jose-javier.castejon.sanchez@biztribution.net>
2024-10-09 00:38:13 +0200

    Update right code

--- ficheros/act03/FicheroXmlSax.java
+++ ficheros/act03/FicheroXmlSax.java
@@ -4,15 +4,22 @@ import java.io.*;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
-
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
 
 public class FicheroXmlSax {
-    public static void main(String[] args) throws FileNotFoundException, IOException, SAXException{
-        XMLReader procesadorXML = XMLReaderFactory.createXMLReader();
-        GestionContenido gestor = new GestionContenido();
-        procesadorXML.setContentHandler(gestor);
-        InputSource archivoXML = new InputSource("./ficheros/act03/alumnos.xml");
-        procesadorXML.parse(archivoXML);
+    public static void main(String[] args) {
+        try {
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            XMLReader procesadorXML = spf.newSAXParser().getXMLReader();
+            GestionContenido gestor = new GestionContenido();
+            procesadorXML.setContentHandler(gestor);
+            InputSource archivoXML = new InputSource(".\\ficheros\\act03\\alumnos.xml");
+            procesadorXML.parse(archivoXML);            
+        } catch (ParserConfigurationException | IOException | SAXException ex) {
+            System.err.printf(ex.getMessage());
+        }
+
+
     }
 }
--- ficheros/act03/GestionContenido.java
+++ ficheros/act03/GestionContenido.java
@@ -9,22 +9,27 @@ public class GestionContenido extends DefaultHandler{
         super();
     }
 
+    @Override
     public void startDocument(){
         System.out.println("Comienzo del documento XML");
     }
 
+    @Override
     public void endDocument(){
         System.out.println("Fin del documento XML");
     }
 
+    @Override
     public void startElement(String uri, String nombre, String nombrec, Attributes atts){
         System.out.printf("\tPrincipio de elemento: %s %n", nombre);
     }
 
+    @Override
     public void endElement(String uri, String nombre, String nombrec){
         System.out.printf("\tFin de elemento: %s %n", nombre);
     }
 
+    @Override
     public void characters(char[] ch, int inicio, int longitud) throws SAXException{
         String car = new String(ch, inicio, longitud);
         car = car.replaceAll("[\t\n]","");  //Eliminar saltos de linea
